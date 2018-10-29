ac73a661b465ccf683d8b53865239df2e3f44d97
DK <dkdan94@gmail.com>
2018-10-29 16:08:42 -0400

    set up timer interval

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const jsContainer = document.getElementById('js');
-jsContainer.innerHTML = `
+const reactContainer = document.getElementById('react');
+
+const render = () => {
+  jsContainer.innerHTML = `
   <div class="demo">
     Hello JS
+    <input />
+    <p>${new Date()}</p>
   </div>
 `;
 
-const reactContainer = document.getElementById('react');
+  ReactDOM.render(
+    React.createElement(
+      'div',
+      { className: 'demo' },
+      'Hello React',
+      React.createElement('input'),
+      React.createElement('p', null, new Date().toString())
+    ),
+    reactContainer
+  );
+};
 
-ReactDOM.render(
-  React.createElement('div', { className: 'demo' }, 'hello react'),
-  reactContainer
-);
-
-//The only major difference between the ways we are building these two nodes in the browser is that in the JS version we used a string to represent the content, while in the React version we used pure JavaScript calls and represented the content with an object instead of a string. No matter how complicated the HTML User Interface is going to get, when using React, every HTML element will be represented with a JavaScript object using a React.createElement call.
+setInterval(render, 1000);
+//We now have two nodes, one being controlled with the DOM Web API directly, and another being controlled with the React API (which in turn uses the DOM Web API). The only major difference between the ways we are building these two nodes in the browser is that in the JS version we used a string to represent the content, while in the React version we used pure JavaScript calls and represented the content with an object instead of a string. No matter how complicated the HTML User Interface is going to get, when using React, every HTML element will be represented with a JavaScript object using a React.createElement call.
